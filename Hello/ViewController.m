4d968b7f517365e2b205d6d76168e1e78b7d4fde
Dustin Langner <dustin.langner1@gmail.com>
2019-01-09 14:47:26 -0800

    Add clickable hello world and other phrases with contraints

--- Hello/ViewController.m
+++ Hello/ViewController.m
@@ -7,17 +7,57 @@
 //
 
 #import "ViewController.h"
+#import <Foundation/Foundation.h>
 
 @interface ViewController ()
+
+@property NSArray *textPhrases;
+@property int textPhraseIndex;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+ 
+    self.textPhraseIndex = 0;
+    self.textPhrases = @[@"Hello World!", @"Go Seahawks!", @"Go Dawgs!", @"Cougs Suck!"];
+    
+    [self setUpHelloWorld];
 }
 
+- (void)setUpHelloWorld
+{
+    // Set state for textDisplay button
+    self.textDisplayButton = [[UIButton alloc] initWithFrame:CGRectZero];
+    [self.textDisplayButton setTitle:self.textPhrases[self.textPhraseIndex] forState:UIControlStateNormal];
+    [self.textDisplayButton setTitleColor:self.view.tintColor forState:UIControlStateNormal];
+    [self.textDisplayButton.titleLabel setFont:[UIFont systemFontOfSize:40.0]];
+    self.textDisplayButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    [self.textDisplayButton addTarget:self action:@selector(displayedTextPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.textDisplayButton.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    // Add button as subiew
+    [self.view addSubview:self.textDisplayButton];
+    
+    // Set autolayout constraints
+    [self.textDisplayButton.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+    [self.textDisplayButton.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
+    [self.textDisplayButton.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = YES;
+    [self.textDisplayButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+}
+     
+     
+- (void)displayedTextPressed:(UIButton *)sender
+{
+    self.textPhraseIndex++;
+    if (self.textPhraseIndex >= self.textPhrases.count) {
+        self.textPhraseIndex = 0;
+    }
+    [self.textDisplayButton setTitle:self.textPhrases[self.textPhraseIndex] forState:UIControlStateNormal];
+}
 
 @end
