745489c592f822b6515651e80b825b945a606e0d
Dustin Langner <dustin.langner1@gmail.com>
2019-01-09 09:10:56 -0800

    Initial Commit

---
+++ Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by Dustin Langner on 1/9/19.
+//  Copyright © 2019 Dustin Langner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by Dustin Langner on 1/9/19.
+//  Copyright © 2019 Dustin Langner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
